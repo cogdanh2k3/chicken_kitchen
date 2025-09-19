1ad31429af10a3253a3227d7dc40ba6a5b8f4e06
Danh <cogdanh2k3@gmail.com>
2025-09-19 10:09:00 +0700

    add empty project with Assets

---
+++ Assets/Scripts/Testting.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Testting : MonoBehaviour
+{
+    private void Start()
+    {
+        if(transform == transform)
+        {
+            Update();
+            for (int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j< 10; j++)
+                {
+                    // Do sth
+                    
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+}
