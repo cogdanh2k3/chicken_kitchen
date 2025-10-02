17129de39a1fa65788f2d5a3e4ef885b7397a2f8
Danh <cogdanh2k3@gmail.com>
2025-10-02 23:49:12 +0700

    Collision Detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,8 +12,49 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerHeight = 2f;
+        float playerRadius = .7f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+        
+        // Cannot move toward moveDir
+        if (!canMove) 
+        {
+            // only X axis
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {   
+                // can move x axis
+                moveDir = moveDirX;
+            }
+            // Cannot move x axis
+            else
+            {
+                // only Z axis
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // cannot move in any direction
+
+                }
+            }
+
+        }
+        
+        if (canMove)
+        {
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
+        
         isWalking = moveDir != Vector3.zero;
 
         float rotationSpeed = 10f;
